dccf0509461f62e9f81b039750eb23b51780f506
ale <alejandro_acosta_rios@hotmail.com>
2021-08-12 16:59:56 -0300

    primer guardado

---
+++ src/main/java/com/clientControl/ClientControlApplication.java
@@ -0,0 +1,16 @@
+package com.clientControl;
+
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientControlApplication.class, args);
+		
+		System.out.println("dasdsadadsda");
+		
+	}
+
+}
---
+++ src/test/java/com/clientControl/PyoyectoWebByteCode1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyoyectoWebByteCode1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
